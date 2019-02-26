5f460a8634878878948349d3a843c4bea1e87553
Yuanzhw <yuanzhw@vip.qq.com>
2019-02-26 22:57:04 +0800

    Remove redundant code

--- utils.py
+++ utils.py
@@ -1,16 +1,13 @@
-import os.path
 import time
 import json
 from flask import make_response
 
 
 def log(*args, **kwargs):
-    # time.time() 返回 unix time
-    # 如何把 unix time 转换为普通人类可以看懂的格式呢？
-    format = '%H:%M:%S'
+    time_format = '%H:%M:%S'
     value = time.localtime(int(time.time()))
-    dt = time.strftime(format, value)
-    with open('gua.log.txt', 'a', encoding='utf-8') as f:
+    dt = time.strftime(time_format, value)
+    with open('log.txt', 'a', encoding='utf-8') as f:
         print(dt, *args, file=f, **kwargs)
 
 
